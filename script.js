2825ec436212d28f85e32c24ab9e8447c46160a3
yanakin394 <yana.germatskaya@gmail.com>
2023-06-10 15:31:21 +0300

    swapi

---
+++ script.js
@@ -0,0 +1,28 @@
+//беру необходимые элементы
+const btn = document.querySelector('btn');
+const selectList = document.getElementById('select').value;
+const number = document.querySelector('number').value;
+const infoBlock = document.querySelector('info');
+
+//создаю функцию для фетч запроса
+function getInfo () {
+    fetch ('http swapi.dev/api/`${selectList}`/${number}/'), {
+        body: JSON.stringify(res),
+    }
+    .then((res) => {
+        console.log(res)
+        infoBlock.innerHTML = res;
+    })
+    .catch((err) => {
+    console.log('Произошла ошибка');
+    });
+}
+
+// добавляю обработчик, вызываю там функцию запроса и ловлю ошибку, если она есть
+btn.addEventListener('click', () => {
+    try {
+        getInfo();
+    } catch(err) {
+        alert('Ошибка сервера!');
+    }
+})
